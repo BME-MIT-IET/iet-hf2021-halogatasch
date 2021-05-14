966e3efba06d84d595918db3b250aec43fd55ffe
Nagy Viktor <52136313+nagyviktor@users.noreply.github.com>
2021-05-15 00:09:26 +0200

    Menu screen tested

--- src/test/java/uitest/UITest.java
+++ src/test/java/uitest/UITest.java
@@ -1,29 +1,76 @@
 package uitest;
 
+import graphics.GameBoard;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import logic.Way;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UITest extends ApplicationTest {
 
     @Before
-    public void startProject() throws Exception{
-        ApplicationTest.launch(graphics.Main.class);
-    }
+    public void startProject() throws Exception{ ApplicationTest.launch(graphics.Main.class); }
     @Override
     public void start(Stage stage){
         stage.show();
     }
+
     @Test
-    public void test()throws Exception{
-        Button b = lookup("#explorer").query();
-        Button b2 = lookup("#explorer").query();
-        Button b3 = lookup("#explorer").query();
-        clickOn(b);
-        clickOn(b2);
-        clickOn(b3);
+    public void testMenuButtons() throws Exception{
+        Button explorer = lookup("#explorer").query();
+        Button eskimo = lookup("#eskimo").query();
+        Button start = lookup("#start").query();
+        //Nem látható
+        clickOn(explorer);
+        clickOn(eskimo);
+        assertEquals(false, start.isVisible());
+        //Látható
+        clickOn(explorer);
+        assertEquals(true, start.isVisible());
+
+        clickOn(eskimo);
+        clickOn(explorer);
+        clickOn(eskimo);
+        //6 karakter után nem megnyomható
+        assertEquals(false, eskimo.isVisible());
+        assertEquals(false, explorer.isVisible());
+    }
+
+    @Test
+    public void testGameBoardBasicButtons() throws Exception{
+        clickOn((Button)lookup("#eskimo").query());
+        clickOn((Button)lookup("#explorer").query());
+        clickOn((Button)lookup("#explorer").query());
+        clickOn((Button)lookup("#start").query());
+
+        //Munka végzés és hp ellenőrzés
+        Label work = lookup("#actionLabel").query();
+        assertEquals("4", work.getText());
+        Button ability = lookup("#ability").query();
+        clickOn(ability);
+        assertEquals("3", work.getText());
+        Label hp = lookup("#bodyHeatLabel").query();
+        assertEquals("3", work.getText());
+
+        //Csákány használatának tiltása
+        Button pickaxe = lookup("#pickaxe").query();
+        clickOn(pickaxe);
+        //assertEquals(false, pickaxe.isVisible());
+
+        //assertEquals(true, );
+        /*Button wayUp = lookup("up").query();
+        Button wayDown = lookup("down").query();
+        Button wayLeft = lookup("left").query();
+        Button wayRight = lookup("right").query();
+        for(Way w : Way.values())
+            clickOn(pass);*/
+
     }
 
 }
