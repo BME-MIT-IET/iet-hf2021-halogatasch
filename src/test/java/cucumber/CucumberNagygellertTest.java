461ba651b002ef66a02b1840aedcce0b2bbb0862
nagygellert <gellert.nagy99@gmail.com>
2021-05-15 01:12:49 +0200

    Cucumber testing almost complete

--- src/test/java/cucumber/CucumberNagygellertTest.java
+++ src/test/java/cucumber/CucumberNagygellertTest.java
@@ -1,10 +1,10 @@
-package logic.characters;
+package cucumber;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "target/test-classes", glue = {"cucumber.steps"})
-public class MyStepTest {
+@CucumberOptions(features = "target/test-classes", glue = {"cucumber"})
+public class CucumberNagygellertTest {
 }
